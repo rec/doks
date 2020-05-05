561f11df237d6d57dcb2966357d1653cb119030d
Tom Ritchford <tom@swirly.com>
2020-05-05 20:58:23 +0200

    Add empty tests and file

---
+++ setup.py
@@ -0,0 +1,31 @@
+from setuptools import setup
+import doks
+
+_classifiers = [
+    'Development Status :: 4 - Beta',
+    'Programming Language :: Python :: 3.5',
+    'Programming Language :: Python :: 3.6',
+    'Programming Language :: Python :: 3.7',
+    'Programming Language :: Python :: 3.8',
+    'Intended Audience :: Developers',
+    'License :: OSI Approved :: MIT License',
+    'Topic :: Software Development :: Libraries',
+    'Topic :: Utilities',
+]
+
+if __name__ == '__main__':
+    setup(
+        name='doks',
+        version=doks.__version__,
+        author='Tom Ritchford',
+        author_email='tom@swirly.com',
+        url='https://github.com/rec/doks',
+        tests_require=['pytest'],
+        py_modules=['doks'],
+        description='Automatically generated READMEs',
+        long_description=open('README.rst').read(),
+        license='MIT',
+        classifiers=_classifiers,
+        keywords=['testing', 'modules'],
+        scripts=['doks.py'],
+    )
---
+++ test_doks.py
@@ -0,0 +1,7 @@
+from unittest import TestCase
+import doks
+
+
+class TestDoks(TestCase):
+    def test_simple(self):
+        assert doks
