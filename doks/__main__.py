caf84488698ecac3b6f842f276156812994c2ed1
Tom Ritchford <tom@swirly.com>
2020-07-08 14:28:52 +0200

    Remove old code from __main__.py

--- doks/__main__.py
+++ doks/__main__.py
@@ -1,5 +1,3 @@
-
-# from . import rst
 from . import doks
 import argparse
 import sys
@@ -12,7 +10,6 @@ def main():
     p.add_argument('source', help=_SOURCE_HELP)
     p.add_argument('target', default=None, nargs='?', help=_TARGET_HELP)
     p.add_argument('--verbose', '-v', action='store_true', help=_VERBOSE_HELP)
-    # p.add_argument('--sections', '-s', default=None, help=_SECTIONS_HELP)
 
     args = p.parse_args()
     try:
@@ -25,7 +22,6 @@ def main():
 
 
 _DESCRIPTION = """Write a .rst document for a single Python file"""
-# _SECTIONS_HELP = 'Characters to use for .rst section headers'
 _SOURCE_HELP = '.py file to create documentation for'
 _TARGET_HELP = '.rst file to write to.  None means stdout'
 _VERBOSE_HELP = """Print more stuff"""
