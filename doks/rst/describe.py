e44306ef25c081a601ee3d0ffe5da31040bcdf0c
Tom Ritchford <tom@swirly.com>
2020-11-06 21:08:56 +0100

    Add describe_doks for custom help messages

--- doks/rst/describe.py
+++ doks/rst/describe.py
@@ -8,52 +8,66 @@ MAX_SIGNATURE = 80
 
 def describe(path, value, sections, is_member, doks):
     section = sections[2 + is_member]
-    if isinstance(value, functools.partial):
-        yield from _describe_partial(path, value, section)
-    else:
-        yield from _describe(path, value, section)
 
+    def describe():
+        try:
+            dok = doks[value]
+        except Exception:
+            dok = doks.get(str(path))
 
-def _describe_partial(path, value, section):
-    yield from rst.header(path, section)
-    yield from ('A partial function:', '')
-    yield from rst.code_block('python')
-    yield '  functools.partial('
-    yield f'      <function {value.func.__name__}>,'
-    yield from (f'      {a!r},' for a in value.args)
-    yield from (f'      {k}={v!r},' for k, v in value.keywords.items())
-    yield from ('  )', '')
+        if dok:
+            return describe_dok(dok)
+        if isinstance(value, functools.partial):
+            return describe_partial()
+        else:
+            return describe_function()
 
+    def describe_dok(dok):
+        yield from signature()
+        yield ''
+        yield dok
+        yield ''
 
-def _describe(path, value, section):
-    doc = inspect.getdoc(value)
-    if not (doc and hasattr(value, '__doc__')):
-        return
-
-    yield from _signature(path, value, section)
-
-    yield code.code(value)
-    yield ''
-
-    yield from doc.splitlines()
-    yield ''
-
-
-def _signature(path, value, section):
-    if isinstance(value, type):
-        line = f'Class ``{path}``'
-        yield from rst.header(line, section)
-        return
-
-    sig = inspect.signature(value)
-    line = f'``{path}{sig}``'
-
-    if len(line) <= MAX_SIGNATURE:
-        yield from rst.header(line, section)
-    else:
-        yield from rst.header(f'``{path}()``', section)
+    def describe_partial():
+        yield from rst.header(path, section)
+        yield from ('A partial function:', '')
         yield from rst.code_block('python')
-        yield f'  {path}('
-        for p in sig.parameters.values():
-            yield f'       {p},'
+        yield '  functools.partial('
+        yield f'      <function {value.func.__name__}>,'
+        yield from (f'      {a!r},' for a in value.args)
+        yield from (f'      {k}={v!r},' for k, v in value.keywords.items())
         yield from ('  )', '')
+
+    def describe_function():
+        doc = inspect.getdoc(value)
+        if not (doc and hasattr(value, '__doc__')):
+            return
+
+        yield from signature()
+
+        yield code.code(value)
+        yield ''
+
+        yield from doc.splitlines()
+        yield ''
+
+    def signature():
+        if isinstance(value, type):
+            line = f'Class ``{path}``'
+            yield from rst.header(line, section)
+            return
+
+        sig = inspect.signature(value)
+        line = f'``{path}{sig}``'
+
+        if len(line) <= MAX_SIGNATURE:
+            yield from rst.header(line, section)
+        else:
+            yield from rst.header(f'``{path}()``', section)
+            yield from rst.code_block('python')
+            yield f'  {path}('
+            for p in sig.parameters.values():
+                yield f'       {p},'
+            yield from ('  )', '')
+
+    yield from describe()
